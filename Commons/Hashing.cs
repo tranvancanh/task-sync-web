4c355eef2a8f60e1553645452bc659011991dc60
tranvancanhtozan <85920372+tranvancanhtozan@users.noreply.github.com>
2023-10-25 13:50:56 +0900

    ハッシュ化とSqlKataを追加

---
+++ Commons/Hashing.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace tec_shipping_management_web.Commons
+{
+    /// <summary>
+    /// ハッシュ化に関する関数
+    /// </summary>
+    /// <remarks>
+    /// 参考:https://qiita.com/Nossa/items/0af4429ceb7628d46909
+    /// </remarks>
+    public static class Hashing
+    {
+        /// <summary>
+        /// 16進数⽂字列をbyte列に変換
+        /// </summary>
+        /// <param name="stringSalt">16進数⽂字列のソルト</param>
+        /// <returns>byte列のソルト</returns>
+        public static byte[] ConvertStringToBytes(string stringSalt)
+        {
+            var listSalt = new List<byte>();
+            for (int i = 0; i < stringSalt.Length / 2; i++)
+            {
+                listSalt.Add(Convert.ToByte(stringSalt.Substring(i * 2, 2), 16));
+            }
+            return listSalt.ToArray();
+        }
+
+
+        /// <summary>
+        /// 平⽂パスワードをハッシュ化されたパスワードに変換
+        /// </summary>
+        /// <param name="plaintext">平文パスワード</param>
+        /// <param name="byteSalt">byte列のソルト</param>
+        /// <returns>16進数⽂字列のパスワード</returns>
+        /// <remarks>
+        /// 参考:https://docs.microsoft.com/ja-jp/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-3.0
+        /// </remarks>
+        public static string ConvertPlaintextPasswordToHashedPassword(string plaintext, byte[] byteSalt)
+        {
+            //Pbkdf2メソッドでハッシュ化する反復回数や作成するハッシュの長さを指定
+            byte[] hash = KeyDerivation.Pbkdf2(
+              plaintext,
+              byteSalt,
+              prf: KeyDerivationPrf.HMACSHA256,
+              iterationCount: 10000,                //反復回数
+              numBytesRequested: 256 / 8);          //ハッシュの長さ
+
+            //ハッシュ値を16進数文字列に変換
+            var hashedPassword = string.Concat(hash.Select(b => $"{b:x2}"));
+            return hashedPassword;
+        }
+
+
+        /// <summary>
+        /// ランダムなソルト取得
+        /// </summary>
+        /// <returns>byte列のソルト</returns>
+        public static byte[] GetRandomSalt()
+        {
+            byte[] byteSalt = new byte[128 / 8];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(byteSalt);
+            return byteSalt;
+        }
+
+
+        /// <summary>
+        /// byte列を16進数⽂字列に変換
+        /// </summary>
+        /// <param name="byteSalt">byte列のソルト</param>
+        /// <returns>16進数⽂字列のソルト</returns>
+        public static string ConvertByteToString(byte[] byteSalt)
+        {
+            return BitConverter.ToString(byteSalt).Replace("-", string.Empty);
+        }
+    }
+}
---
+++ Commons/MssSqlKata.cs
@@ -0,0 +1,61 @@
+﻿using SqlKata.Compilers;
+using SqlKata.Execution;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace task_sync_web.Commons.MssSqlKata
+{
+    public class MssSqlKata : QueryFactory, IDisposable
+    {
+        private static readonly string connectionString = new GetConnectString("tasksync_0_test").ConnectionString;
+
+        private IDbTransaction _transaction = null;
+
+        public MssSqlKata() 
+        {
+            var connection = new SqlConnection(connectionString);
+            var compiler = new SqlServerCompiler();
+            this.Connection = connection;
+            this.Compiler = compiler;
+        }
+
+        public IDbTransaction Begin()
+        {
+            if (this.Connection is not null)
+                return _transaction = this.Connection.BeginTransaction();
+            else
+                throw new ArgumentNullException("Connection is null");
+        }
+
+        public void Commit()
+        {
+            try
+            {
+                if (_transaction is not null)
+                    _transaction.Commit();
+                else
+                    throw new ArgumentNullException("Transaction is null");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return;
+        }
+
+        public void Rollback()
+        {
+            if (_transaction is not null)
+                _transaction.Rollback();
+            else
+                throw new ArgumentNullException("Transaction is null");
+            return;
+        }
+
+        public new void Dispose()
+        {
+            base.Dispose();
+        }
+
+    }
+}
