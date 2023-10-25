b006dc3f9fda89257221d523641f7ade7b27f360
tranvancanhtozan <85920372+tranvancanhtozan@users.noreply.github.com>
2023-10-25 09:06:14 +0900

    パスワード変更：1回目

---
+++ Controllers/PasswordChangeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task_sync_web.Models;
+
+namespace task_sync_web.Controllers
+{
+    public class PasswordChangeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var userName = User.Identity.Name;
+            var userID = "10001";
+            var changePassword = new ChangePasswordViewModel()
+            {
+                CurrentUserCode = userName,
+                CurrentPass = string.Empty,
+                NewPass = string.Empty,
+                ConfirmPass = string.Empty
+            };
+            return View(changePassword);
+        }
+
+        [HttpPost]
+        public IActionResult Index(ChangePasswordViewModel changePassword)
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace task_sync_web.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [Display(Name = "現在のユーザー")]
+        public string CurrentUserCode { get; set; }
+
+        [Display(Name = "現在のパスワード入力")]
+        public string CurrentPass { get; set; }
+
+        [Display(Name = "新しいパスワード入力")]
+        public string NewPass { get; set; }
+
+        [Display(Name = "新しいパスワード入力(確認用)")]
+        public string ConfirmPass { get; set; }
+    }
+}
