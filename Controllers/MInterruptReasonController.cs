32fdb944988f16ccd737579cfbe0940768d287b4
tranvancanhtozan <85920372+tranvancanhtozan@users.noreply.github.com>
2023-10-31 18:12:11 +0900

    中断理由マスター：3回目(masterからマジ)

--- Controllers/MInterruptReasonController.cs
+++ Controllers/MInterruptReasonController.cs
@@ -7,15 +7,14 @@ using X.PagedList;
 namespace task_sync_web.Controllers
 {
 
-    public class MInterruptReasonController : Controller
+    public class MInterruptReasonController : BaseController
     {
         [HttpGet]
         public IActionResult Index(MInterruptReasonViewModel viewModel, int? pageNumber, string mess = null)
         {
             try
             {
-                var dbName = User.Claims.Where(x => x.Type == CustomClaimTypes.ClaimType_CompanyDatabaseName).First().Value;
-                var listInterruptReason = GetListInterruptReason(viewModel.SearchKeyWord, dbName);
+                var listInterruptReason = GetListInterruptReason(viewModel.SearchKeyWord);
                 if (!listInterruptReason.Any())
                 {
                     ViewData["ErrorMessage"] = ErrorMessages.EW0102;
@@ -53,10 +52,6 @@ namespace task_sync_web.Controllers
         {
             try
             {
-                var dbName = User.Claims.Where(x => x.Type == CustomClaimTypes.ClaimType_CompanyDatabaseName).First().Value;
-                var administratorId = User.Claims.Where(x => x.Type == CustomClaimTypes.ClaimType_AdministratorId).First().Value;
-                var efftedRows = -1;
-
                 if (string.IsNullOrWhiteSpace(model.InterruptReasonCode))
                     model.InterruptReasonCode = string.Empty;
 
@@ -64,15 +59,15 @@ namespace task_sync_web.Controllers
                     model.InterruptReasonName = string.Empty;
 
                 // update
-                using (var db = new DbSqlKata(dbName))
+                using (var db = new DbSqlKata(LoginUser.CompanyDatabaseName))
                 {
-                    efftedRows = db.Query("MInterruptReason").Where("InterruptReasonId", model.InterruptReasonId).Update(new
+                    var efftedRows = db.Query("MInterruptReason").Where("InterruptReasonId", model.InterruptReasonId).Update(new
                     {
                         InterruptReasonCode = model.InterruptReasonCode,
                         InterruptReasonName = model.InterruptReasonName,
                         IsNotUse = model.IsNotUse,
                         UpdateDateTime = DateTime.Now.Date,
-                        UpdateAdministratorId = administratorId
+                        UpdateAdministratorId = LoginUser.AdministratorId
                     });
                     if (efftedRows > 0)
                         return RedirectToAction("Index", new
@@ -110,11 +105,11 @@ namespace task_sync_web.Controllers
             }
         }
 
-        private List<MInterruptReasonModel> GetListInterruptReason(string searchKey, string dbName)
+        private List<MInterruptReasonModel> GetListInterruptReason(string searchKey)
         {
             var interruptReasonModels = new List<MInterruptReasonModel>();
             var listInterruptReason = new List<MInterruptReasonModel>();
-            using (var db = new DbSqlKata(dbName))
+            using (var db = new DbSqlKata(LoginUser.CompanyDatabaseName))
             {
                 listInterruptReason = db.Query("MInterruptReason as a")
                 .Select(
