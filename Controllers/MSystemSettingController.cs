11540f5d25808bb693949de0114fb289206c02ec
c_yamamoto <c_yamamoto@tozan.co.jp>
2023-10-31 13:39:25 +0900

    refactor:システム設定　MAdministrator(Base)と処理や変数名、レイアウトを同じにする修正。モーダルのデザインを変更。必要な項目を追加。

--- Controllers/MSystemSettingController.cs
+++ Controllers/MSystemSettingController.cs
@@ -7,18 +7,17 @@ using X.PagedList;
 
 namespace task_sync_web.Controllers
 {
-    [Authorize]
     public class MSystemSettingController : BaseController
     {
         [HttpGet]
-        public IActionResult Index(MSystemSettingViewModel viewModel, int? pageNumber, string mess = null)
+        public IActionResult Index(MSystemSettingViewModel viewModel, int? pageNumber, string message = null)
         {
             try
             {
                 var listSetting = GetListMAdministrator(viewModel.SearchKeyWord);
                 if (!listSetting.Any())
                 {
-                    ViewData["MessageError"] = ErrorMessages.EW0102;
+                    ViewData["ErrorMessage"] = ErrorMessages.EW0102;
                     return View(viewModel);
                 }
 
@@ -26,15 +25,20 @@ namespace task_sync_web.Controllers
                 var listPaged = listSetting.ToPagedList(pageNumber ?? 1, viewModel.PageRowCount);
                 viewModel.SystemSettingModels = listPaged;
 
-                if (!string.IsNullOrEmpty(mess))
+                if (!string.IsNullOrEmpty(message))
                 {
-                    ViewData["MessageSuccess"] = mess;
+                    ViewData["SuccessMessage"] = message;
                 }
                 return View(viewModel);
             }
-            catch
+            catch (CustomExtention ex)
             {
-                ViewData["MessageError"] = ErrorMessages.EW500;
+                ViewData["ErrorMessage"] = ex.Message;
+                return View(viewModel);
+            }
+            catch (Exception ex)
+            {
+                ViewData["ErrorMessage"] = ErrorMessages.EW500;
                 return View(viewModel);
             }
         }
@@ -51,7 +55,7 @@ namespace task_sync_web.Controllers
                         settingModel.SystemSettingStringValue = string.Empty;
                     var efftedRows = db.Query("MSystemSetting").Where("SystemSettingId", settingModel.SystemSettingId).Update(new
                     {
-                        SystemSettingStringValue = settingModel.SystemSettingStringValue,
+                        settingModel.SystemSettingStringValue,
                         UpdateDateTime = DateTime.Now.Date,
                         UpdateAdministratorId = LoginUser.AdministratorId
                     });
@@ -68,49 +72,48 @@ namespace task_sync_web.Controllers
             }
         }
 
-        private List<MSystemSettingModel> GetListMAdministrator(string keySearch)
+        private List<MSystemSettingModel> GetListMAdministrator(string searchKey)
         {
-            var list = new List<MSystemSettingModel>();
+            var systemSettingModels = new List<MSystemSettingModel>();
             using (var db = new DbSqlKata(LoginUser.CompanyDatabaseName))
             {
-                if (string.IsNullOrWhiteSpace(keySearch))
+                // DBからデータ一覧を取得
+                var systemSettingList = db.Query("MSystemSetting")
+                .Select(
+                    "MSystemSetting.SystemSettingId",
+                    "MSystemSetting.SystemSettingOutline",
+                    "MSystemSetting.SystemSettingDetail",
+                    "MSystemSetting.SystemSettingValue",
+                    "MSystemSetting.SystemSettingStringValue",
+                    "MSystemSetting.UpdateDateTime",
+                    "MAdministrator.AdministratorLoginId as UpdateAdministratorId",
+                    "MAdministrator.AdministratorName as UpdateAdministratorName"
+                    )
+                .LeftJoin("MAdministrator", "MSystemSetting.UpdateAdministratorId", "MAdministrator.AdministratorId")
+                .OrderBy("MSystemSetting.SystemSettingId")
+                .Get<MSystemSettingModel>().ToList();
+
+                if (systemSettingList.Count == 0)
                 {
-                    var list1 = db.Query("MSystemSetting")
-                    .Select(
-                        "MSystemSetting.SystemSettingId",
-                        "MSystemSetting.SystemSettingOutline",
-                        "MSystemSetting.SystemSettingDetail",
-                        "MSystemSetting.SystemSettingStringValue",
-                        "MSystemSetting.UpdateDateTime",
-                        "MAdministrator.AdministratorLoginId as UpdateAdministratorId",
-                        "MAdministrator.AdministratorName as UpdateAdministratorName"
-                        )
-                    .LeftJoin("MAdministrator", "MSystemSetting.UpdateAdministratorId", "MAdministrator.AdministratorId")
-                    .WhereNotNull("MAdministrator.AdministratorName")
-                    .OrderBy("MSystemSetting.SystemSettingId")
-                    .Get<MSystemSettingModel>();
+                    throw new CustomExtention(ErrorMessages.EW0101);
+                }
+
+                searchKey = (searchKey ?? "").Trim();
+                if (systemSettingList.Count > 0 && searchKey.Length > 0)
+                {
+                    // 検索キーワードが存在する場合
+                    systemSettingModels = systemSettingList.Where(x => x.SystemSettingId.ToString().Contains(searchKey) || x.SystemSettingOutline.Contains(searchKey)).ToList();
+                    if (systemSettingModels.Count == 0)
+                    {
+                        throw new CustomExtention(ErrorMessages.EW0102);
+                    }
                 }
                 else
                 {
-                    list = db.Query("MSystemSetting")
-                    .Select(
-                        "MSystemSetting.SystemSettingId",
-                        "MSystemSetting.SystemSettingOutline",
-                        "MSystemSetting.SystemSettingDetail",
-                        "MSystemSetting.SystemSettingStringValue",
-                        "MSystemSetting.UpdateDateTime",
-                        "MAdministrator.AdministratorLoginId as UpdateAdministratorId",
-                        "MAdministrator.AdministratorName as UpdateAdministratorName"
-                        )
-                    .LeftJoin("MAdministrator", "MSystemSetting.UpdateAdministratorId", "MAdministrator.UpdateAdministratorId")
-                    .WhereNotNull("MAdministrator.AdministratorName")
-                    .WhereLike("SystemSettingId", $"%{keySearch}%")
-                    .OrWhereLike("SystemSettingOutline", $"%{keySearch}%")
-                    .OrderBy("MSystemSetting.SystemSettingId")
-                    .Get<MSystemSettingModel>().ToList();
+                    systemSettingModels = systemSettingList;
                 }
+                return systemSettingModels;
             }
-            return list;
         }
     }
 }
