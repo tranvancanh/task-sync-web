2e6c60af1a998e6181d463df6fbd56286b41ce9e
千鶴 山本 <c_yamamoto@tozan.co.jp>
2023-11-14 09:54:33 +0900

    feat: アプリダウンロードページ追加

--- Models/MCompanyModel.cs
+++ Models/MCompanyModel.cs
@@ -5,5 +5,7 @@ namespace task_sync_web.Models
         public int CompanyId { get; set; }
         public string CompanyName { get; set; }
         public string CompanyDatabaseName { get; set; }
+        public decimal SmartphoneAppMinVersion { get; set; }
+        public string SmartphoneAppDownloadCode { get; set; }
     }
 }
