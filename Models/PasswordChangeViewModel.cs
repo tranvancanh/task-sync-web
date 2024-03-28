28f73b7fc8274f2ad200c2bf87d5eb634f485f3c
MyMV <hoangmy1992@gmail.com>
2024-03-28 11:48:26 +0900

    fix: パスワードの複雑さを検証する

--- Models/PasswordChangeViewModel.cs
+++ Models/PasswordChangeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace task_sync_web.Models
 {
@@ -11,15 +12,38 @@ namespace task_sync_web.Models
 
         [Display(Name = "新しいパスワード")]
         [Required(ErrorMessageResourceName = "EW0001", ErrorMessageResourceType = typeof(ErrorMessages))]
-        [RegularExpression(@"[a-zA-Z0-9]+", ErrorMessageResourceName = "EW0010", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [RegularExpression("^[!-~]+$", ErrorMessageResourceName = "EW0010", ErrorMessageResourceType = typeof(ErrorMessages))]
         [StringLength(12, MinimumLength = 8, ErrorMessageResourceName = "EW0003", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [CustomValidation(typeof(PasswordChangeViewModel), nameof(ValidatePasswordComplexity))]
         public string NewPassword { get; set; }
 
         [Display(Name = "新しいパスワード(確認用)")]
         [Required(ErrorMessageResourceName = "EW0001", ErrorMessageResourceType = typeof(ErrorMessages))]
-        [RegularExpression(@"[a-zA-Z0-9]+", ErrorMessageResourceName = "EW0010", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [RegularExpression("^[!-~]+$", ErrorMessageResourceName = "EW0010", ErrorMessageResourceType = typeof(ErrorMessages))]
         [StringLength(12, MinimumLength = 8, ErrorMessageResourceName = "EW0003", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [CustomValidation(typeof(PasswordChangeViewModel), nameof(ValidatePasswordComplexity))]
         public string ConfirmNewPassword { get; set; }
+
+        /// <summary>
+        /// パスワードの複雑さを検証する
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static ValidationResult ValidatePasswordComplexity(string password, ValidationContext context)
+        {
+            if (string.IsNullOrEmpty(password)) return ValidationResult.Success;
+
+            bool hasUpperCase = password.Any(char.IsUpper);
+            bool hasLowerCase = password.Any(char.IsLower);
+            bool hasDigits = password.Any(char.IsDigit);
+            bool hasSymbols = password.Any(ch => !char.IsLetterOrDigit(ch));
+            int typeCount = new[] { hasUpperCase, hasLowerCase, hasDigits, hasSymbols }.Count(t => t);
+
+            if (typeCount >= 2)
+                return ValidationResult.Success;
+            else
+                return new ValidationResult("パスワードは数字、大文字・小文字の英字、記号のうち2種以上の組合せである必要があります。");
+        }
 
         public PasswordChangeViewModel()
         {
